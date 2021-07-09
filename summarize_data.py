00560e91296c1558c4e89cb4bf6b2c387bd6ace0
Chawanat Nakasan <chawanat.n@gmail.com>
2021-07-09 23:55:40 +0900

    finalize data and reports

--- summarize_data.py
+++ summarize_data.py
@@ -4,11 +4,21 @@ import os
 
 df_s = pd.read_csv(os.path.join('data', 'revised_school_table.csv'), encoding='utf_8')
 
-df_s = df_s.groupby(['region', 'new.area.code', 'size'])
+def sorter(L):
+    return dict(zip(L, range(len(L))))
 
-summary_teachers = df_s.sum()[['number.teachers']].unstack().fillna(0).astype('int32')
+#region_order = list('BCNIS')
+#region_order_sorter = sorter(region_order)
+#size_order = list('xlms')
+#size_order_sorter = sorter(size_order)
+
+summary_teachers_by_region = df_s.sort_values(['region', 'new.area.code', 'size']).set_index('region').groupby(['size','region'])[['sum.execs','sum.teachers']].sum().fillna(0).astype('int32')
+summary_teachers_by_region.to_csv(os.path.join('reports', 'report_teachers_by_region.csv'), encoding='utf_8', index=True)
+
+# Right part of Table 1 (teacher count by region without size discrimination)
+summary_teachers = df_s.sort_values(['region', 'new.area.code']).groupby(['region', 'new.area.code'])[['sum.execs','count.teacher','count.assistant']].sum().fillna(0).astype('int32')
 summary_teachers.to_csv(os.path.join('reports', 'report_teachers.csv'), encoding='utf_8', index=True)
 
-summary_count = df_s.count()[['code.smis']].unstack().fillna(0).astype('int32')
+summary_count = df_s.sort_values(['region', 'new.area.code', 'size']).groupby(['region', 'new.area.code', 'size'])[['code.ministry']].count().unstack().fillna(0).astype('int32')
 summary_count.to_csv(os.path.join('reports', 'report_count.csv'), encoding='utf_8', index=True)
 
